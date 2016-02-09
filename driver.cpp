3d57755e4d00c54e2f2ce1e76f57531f15f0e071
Austin DeLauney <austin@Austins-MacBook-Pro.local>
2016-02-08 19:29:23 -0500

    File stuff now works

--- driver.cpp
+++ driver.cpp
@@ -2,6 +2,7 @@
 #include "Continent.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 
 using namespace std;
@@ -17,9 +18,38 @@ int main()
 	dataFile.open("2013WorldBankEducationCensusData.txt");
 
 	string line = "";
+
+	stringstream test("Hello What Is Up?");
+	cout << test << endl;
+	test.str("HHHH");
+	cout << "HELLLOOOOOOOOOOO";
+	cout << endl;
+
 	while (getline(countriesFile, line))
 	{
 		if(line.find("--") != std::string::npos)
+		{
 			cout << "Found -- in " << line << endl;
+			istringstream isis(line);
+			string continent, filler;
+			int countriesAmount;
+			isis >> continent >> filler >> countriesAmount;
+			//cout << continent << filler << countriesAmount << endl;
+			vector<Country> countries;
+			for (int i = 0; i < countriesAmount; i++)
+			{
+				string temp_line;
+				getline(countriesFile, temp_line);
+				//isis.str(getline(countriesFile, temp_line));
+				istringstream temp_isis(temp_line);
+				cout << temp_line << endl;
+				countries.push_back(Country("Test", -1, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0));
+			}
+			//cout << countries;
+			world.push_back(Continent(continent, countries));
+		}
+
+		else
+			cout << "ERROR ERROR ERROR" << endl;
 	}
 }
